9db1f8f4240ebe491349bd40c3758d138e5a492b
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2021-10-26 06:47:48 -0300

    Update fools.js

--- js/fools.js
+++ js/fools.js
@@ -412,7 +412,7 @@ setTimeout(function(){
   vidd2.play();
   vidd2.style.opacity = "1"
   endBTN.setAttribute("onclick", "closeAll()");
-}, 500)
+}, 1500)
 
 function closeAll(){
   var screenS = screen.width;
