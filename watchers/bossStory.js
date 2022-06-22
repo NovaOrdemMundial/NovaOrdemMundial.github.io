3d6a9573fd7f2628d0fbb800fa4f5c8cad726937
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-06-22 04:55:14 -0300

    Update bossStory.js

--- watchers/bossStory.js
+++ watchers/bossStory.js
@@ -1,131 +1,259 @@
 //////            //      Historico de boss      //          ////////////
 
 const FoolsStory = [
-    combat1 = {
-      player: 'Fools113',
-      action: 'Derrotou',
-      victim: 'RockeyDoggy'
-    },
-    combat2 = {
-      player: 'Fools113',
-      action: '✬',
-      victim: 'Se Tornou Boss'
-    },
-    combat3 = {
-      player: 'rGreque',
-      action: 'Derrotou',
-      victim: 'Fools113'
-    },
-    combat4 = {
-      player: 'Fools113',
-      action: '+50HP',
-      victim: 'Se Curou'
-    },
-    combat5 = {
-      player: 'RafaaSmile',
-      action: '-0.21HP',
-      victim: 'Fools113'
-    },
-    combat6 = {
-      player: 'Sbevee_',
-      action: '-3.48HP',
-      victim: 'Fools113'
-    },
-    combat7 = {
-      player: 'Fools113',
-      action: 'Derrotou',
-      victim: 'rGreque'
-    },
-    combat8 = {
-      player: 'Fools113',
-      action: '✬',
-      victim: 'Se Tornou Boss'
-    },
-    combat9 = {
-      player: 'Draaaxxy',
-      action: '-5HP',
-      victim: 'Fools113'
-    },
-    combat10 = {
-      player: 'Fools113',
-      action: '♚',
-      victim: 'Venceu'
-    },
-    combat11 = {
-      player: 'Fools113',
-      action: '-20HP',
-      victim: 'Sbeeker'
-    },
-  ]
-  
-  const JuninhoStory = [
-    combat1 = {
-      player: 'rGreque',
-      action: 'Derrotou',
-      victim: 'Fools113'
-    },
-    combat2 = {
-      player: 'rGreque',
-      action: '✬',
-      victim: 'Se Tornou Boss'
-    },
-    combat3 = {
-      player: 'Fools113',
-      action: '-50HP',
-      victim: 'rGreque'
-    },
-    combat4 = {
-      player: 'Fools113',
-      action: 'Derrotou',
-      victim: 'rGreque'
-    },
-  ]
-  
-  const KingStory = [
-    combat1 = {
-      player: 'S1rKing_',
-      action: '-1.11HP',
-      victim: 'Sbeeker'
-    },
-    combat2 = {
-      player: 'S1rKing_',
-      action: '-1HP',
-      victim: 'Se Tornou Boss'
-    },
-    combat3 = {
-      player: 'S1rKing_',
-      action: 'Derrotou',
-      victim: 'Kinguinha'
-    },
-  ]
-  
-  const PandasStory = [
-    combat1 = {
-      player: 'Pandas_S2',
-      action: 'Derrotou',
-      victim: 'Lucariojoga'
-    },
-  ]
-  
-  const GuiStory = [
-    combat1 = {
-      player: 'godiira',
-      action: 'Derrotou',
-      victim: 'Pandas_S2'
-    },
-  ]
-  
-  const EdneyStory = [
-    combat1 = {
-      player: 'edney_de_pijama',
-      action: 'Derrotou',
-      victim: 'godiira'
-    },
-    combat2 = {
-      player: 'edney_de_pijama',
-      action: '+749HP',
-      victim: 'Se Curou'
-    },
-  ]
+  combat090 = {
+    player: 'Fools113',
+    action: '-20HP',
+    victim: 'Sbeeker'
+  },
+  combat091 = {
+    player: 'Fools113',
+    action: '♚',
+    victim: 'Venceu'
+  },
+  combat092 = {
+    player: 'Draaaxxy',
+    action: '-5HP',
+    victim: 'Fools113'
+  },
+  combat093 = {
+    player: 'Fools113',
+    action: '✬',
+    victim: 'Se Tornou Boss'
+  },
+  combat094 = {
+    player: 'Fools113',
+    action: 'Derrotou',
+    victim: 'rGreque'
+  },
+  combat095 = {
+    player: 'Sbevee_',
+    action: '-3.48HP',
+    victim: 'Fools113'
+  },
+  combat096 = {
+    player: 'RafaaSmile',
+    action: '-0.21HP',
+    victim: 'Fools113'
+  },
+  combat097 = {
+    player: 'Fools113',
+    action: '+50HP',
+    victim: 'Se Curou'
+  },
+  combat098 = {
+    player: 'rGreque',
+    action: 'Derrotou',
+    victim: 'Fools113'
+  },
+  combat099 = {
+    player: 'Fools113',
+    action: '✬',
+    victim: 'Se Tornou Boss'
+  },
+  combat100 = {
+    player: 'Fools113',
+    action: 'Derrotou',
+    victim: 'RockeyDoggy'
+  },
+]
 
-  //////            //      Historico dos não Boss      //          ////////////
+const JuninhoStory = [
+  combat1 = {
+    player: 'Fools113',
+    action: 'Derrotou',
+    victim: 'rGreque'
+  },
+  combat2 = {
+    player: 'Fools113',
+    action: '-50HP',
+    victim: 'rGreque'
+  },
+  combat3 = {
+    player: 'rGreque',
+    action: '✬',
+    victim: 'Se Tornou Boss'
+  },
+  combat4 = {
+    player: 'rGreque',
+    action: 'Derrotou',
+    victim: 'Fools113'
+  },
+]
+
+const KingStory = [
+  combat1 = {
+    player: 'S1rKing_',
+    action: 'Derrotou',
+    victim: 'Kinguinha'
+  },
+  combat2 = {
+    player: 'S1rKing_',
+    action: '-1HP',
+    victim: 'Se Tornou Boss'
+  },
+  combat3 = {
+    player: 'S1rKing_',
+    action: '-1.11HP',
+    victim: 'Sbeeker'
+  },
+]
+
+const PandasStory = [
+  combat099 = {
+    player: 'Pandas_S2',
+    action: 'Derrotou',
+    victim: 'Lucariojoga'
+  },
+  combat100 = {
+    player: 'Pandas_S2',
+    action: '-100HP',
+    victim: 'Lucariojoga'
+  },
+]
+
+const GuiStory = [
+  combat099 = {
+    player: 'godiira',
+    action: 'Derrotou',
+    victim: 'Pandas_S2'
+  },
+  combat100 = {
+    player: 'godiira',
+    action: '-100HP',
+    victim: 'Pandas_S2'
+  },
+]
+
+const EdneyStory = [
+  combat098 = {
+    player: 'edney_de_pijama',
+    action: '+749HP',
+    victim: 'Se Curou'
+  },
+  combat099 = {
+    player: 'edney_de_pijama',
+    action: 'Derrotou',
+    victim: 'godiira'
+  },
+  combat100 = {
+    player: 'edney_de_pijama',
+    action: '-759HP',
+    victim: 'godiira'
+  },
+]
+
+/////////       /////  Não Bosses Historico  /////       //////////
+const SbeekerStory = [
+  combat094 = {
+    player: 'WhityKun_',
+    action: '-3HP',
+    victim: 'Sbeeker'
+  },
+  combat095 = {
+    player: 'WhityKun_',
+    action: '-5.61HP',
+    victim: 'Sbeeker'
+  },
+  combat096 = {
+    player: 'Fools113',
+    action: '-20HP',
+    victim: 'Sbeeker'
+  },
+  combat097 = {
+    player: 'S1rKing_',
+    action: '-1HP',
+    victim: 'Sbeeker'
+  },
+  combat098 = {
+    player: 'S1rKing_',
+    action: '-1.11HP',
+    victim: 'Sbeeker'
+  },
+  combat099 = {
+    player: 'GirlStrangerFear',
+    action: '-1.5HP',
+    victim: 'Sbeeker'
+  },
+  combat100 = {
+    player: 'Sbeeker',
+    action: '👁',
+    victim: 'Boss Misterioso'
+  },
+  combat100 = {
+    player: 'AAAA',
+    action: 'AAA',
+    victim: 'AAAA'
+  },
+]
+
+const SteveStory = [
+  combat100 = {
+    player: 'Sbevee_',
+    action: '-3.48HP',
+    victim: 'Fools113'
+  },
+]
+
+const RafaStory = [
+  combat100 = {
+    player: 'RafaaSmile',
+    action: '-0.21HP',
+    victim: 'Fools113'
+  },
+]
+
+const MandraxStory = [
+  combat100 = {
+    player: 'Draaaxxy',
+    action: '-5HP',
+    victim: 'Fools113'
+  },
+]
+
+const GirlStory = [
+  combat100 = {
+    player: 'GirlStrangerFear',
+    action: '-1.5HP',
+    victim: 'Sbeeker'
+  },
+]
+
+const WhityStory = [
+  combat099 = {
+    player: 'WhityKun_',
+    action: '-5.61HP',
+    victim: 'Sbeeker'
+  },
+  combat100 = {
+    player: 'WhityKun_',
+    action: '-3HP',
+    victim: 'Sbeeker'
+  },
+]
+
+const KingaStory = [
+  combat098 = {
+    player: 'S1rKing_',
+    action: '-70HP',
+    victim: 'Kinguinha'
+  },
+  combat099 = {
+    player: 'Kinguinha',
+    action: 'Derrotou',
+    victim: 'RockeyDoggy'
+  },
+  combat100 = {
+    player: 'Kinguinha',
+    action: '-30HP',
+    victim: 'RockeyDoggy'
+  },
+]
+
+const PauloStory = [
+  combat100 = {
+    player: 'Top 3',
+    action: '_',
+    victim: '524HP'
+  },
+]
