b33b8a7f96f74d2d36b55e8cbbe8016c6a7f842f
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-06-29 03:48:54 -0300

    Create oMeioPraBaixo.js

---
+++ watchers/chxoConfidências/oMeioPraBaixo.js
@@ -0,0 +1,13 @@
+//DataBase
+const firebaseConfig = {
+  apiKey: "AIzaSyCK-Hh9WOIjhNzQYtnAg-veYQaHmsN5nvQ",
+  authDomain: "projeto-de-teste-3b179.firebaseapp.com",
+  projectId: "projeto-de-teste-3b179",
+  storageBucket: "projeto-de-teste-3b179.appspot.com",
+  messagingSenderId: "1020870468830",
+  appId: "1:1020870468830:web:36313c872dc8a7a2c10e4f",
+  measurementId: "G-ZFEK85WY4H"
+};
+firebase.initializeApp(firebaseConfig);
+
+const path = firebase.firestore();
