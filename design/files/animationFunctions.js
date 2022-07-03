399f0943b397f9063429592ee8a80a86687a4d2f
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-07-03 07:15:19 -0300

    Update animationFunctions.js

--- design/files/animationFunctions.js
+++ design/files/animationFunctions.js
@@ -16,3 +16,11 @@ function ifNotMatch(){
     checkStatus.style.setProperty('--animationStateIs', 'paused');
   }, 8000)
 }
+
+//Botão Sacrificar Tudo
+SacrificeALL.addEventListener('mouseover', () => {
+  SacrificeALL.style.setProperty('--sacrificeAnimation', 'sacrificeRotate');
+});
+SacrificeALL.addEventListener('mouseout', () => {
+  SacrificeALL.style.setProperty('--sacrificeAnimation', 'none');
+});
