0cd5040a7beff296e42ceb01f9df8fd2f7010a67
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-07-15 05:23:47 -0300

    Update mapDesign.js

--- mechanics/draw/mapDesign.js
+++ mechanics/draw/mapDesign.js
@@ -1,9 +1,10 @@
 //Get Hooked
-const ron   = 'https://discord';
-const nal   = '.com/api/webhooks';
-const dinho = '/982912777792';
-const soc   = '397332/';
-const cer   = 'Axv0bcjSq';
-const game  = 'tTfY38Q4bHZu_1nZPJ8cKniC22qZBLkXtQXg0oC';
-const play  = '_vLzlpDmgt7DJa16gUi0';
-
+const ron       = 'https://discord';
+const nal       = '.com/api/webhooks';
+const dinho     = '/982912777792';
+const soc       = '397332/';
+const cer       = 'Axv0bcjSq';
+const game      = 'tTfY38Q4bHZu_1nZPJ8cKniC22qZBLkXtQXg0oC';
+const play      = '_vLzlpDmgt7DJa16gUi0';
+const botName   = 'The Eye in One Mind';
+const botAvatar = '';
