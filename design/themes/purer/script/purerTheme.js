8db2159c873f430e28d19166b1ebc774c8844673
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-07-20 03:42:29 -0300

    Create purerTheme.js

---
+++ design/themes/purer/script/purerTheme.js
@@ -0,0 +1,34 @@
+
+////////////__||Temas||__////////////
+const wallpaperImg = document.createElement('IMG');
+//Pures
+function purer(){
+  wallpaperImg.src = 'https://i.imgur.com/YokRXV5.png';//
+  wallpaperImg.setAttribute('style', 'filter:sepia(50%) hue-rotate(-10deg) contrast(60%);');
+  menuMapsGuide.setAttribute('class', 'purers');
+  slidePanel.append(wallpaperImg);
+  for(var t = 0; t < bossBox.length; t++){
+    bossBox[t].classList.add("purerBoxTheme");//
+  }
+  olhoAberto.src     = 'https://novaordemmundial.github.io/design/themes/purer/tooned/Sunflower%20Eye%20Looped.webm';
+  olharCensura  .src = 'https://novaordemmundial.github.io/design/themes/purer/tooned/Sunflower%20Eye%20Censure.webm';
+  olhoAbrindo   .src = 'https://novaordemmundial.github.io/design/themes/purer/tooned/Sunflower%20Eye%20Opening.webm';
+  olhoDesaparece.src = 'https://novaordemmundial.github.io/design/themes/purer/tooned/Sunflower%20Eye%20Dissolve.webm';
+  menuBotao.classList.add('purerButtonTheme');
+  wallpaperVignetta.style.backgroundImage = 'radial-gradient(#1110 15%, rgba(15,9,5,0.95) 65%)';
+}
+
+function clearPurer(){
+  menuMapsGuide.removeAttribute('class');
+  slidePanel.innerHTML = '';
+  for(var t = 0; t < bossBox.length; t++){
+    bossBox[t].classList.remove("purerBoxTheme");//
+  }
+  olhoAberto    .src = abertoLoopLink;
+  olharCensura  .src = olharCensuraLink;
+  olhoAbrindo   .src = abrirOlhoLink;
+  olhoDesaparece.src = olhoDesapareceLink;
+  menuBotao.classList.remove('purerButtonTheme');
+  wallpaperVignetta.style.backgroundImage = '';
+  
+}
