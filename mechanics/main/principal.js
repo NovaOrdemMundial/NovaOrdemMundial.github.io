82a124fdbab3315a8aa428f89989747353d37413
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-08-01 09:06:14 -0300

    Delete mechanics directory

--- mechanics/main/principal.js
+++
@@ -1,9 +0,0 @@
-﻿algumascoisinhas();
-
-//    //  /// Olho No Fundo ///   ///       ///
- const eye = document.querySelector('#eyeIris');
- window.addEventListener('mousemove', (event) => {
- const x = -(window.innerWidth / 2 - event.pageX) / 7;
- const y = -(window.innerHeight / 2 - event.pageY) / 7;
- eye.style.transform = `translateY(${y}px) translateX(${x}px)`;         
-}); 
