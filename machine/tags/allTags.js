5dabae72c0ff9b3fe3dc769732be6fa0f4b05277
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-08-02 05:30:22 -0300

    Update allTags.js

--- machine/tags/allTags.js
+++ machine/tags/allTags.js
@@ -186,28 +186,6 @@ const itemUseResult = document.createElement('INPUT');  itemUseResult .setAttrib
 ///Sistema de Highlights///
 const slidePanel = document.getElementById('slide');
 
-///////////////////
-
-///////Notifications//////////
-const acgsfxNotify = new Audio(classicFightSFX.notification);
-
-
-//////////////////////////////////
-
-//////////////////SECRETS OF ABOUT US///////////// < -- Arquivo De Senhas
-const debugHave               = localStorage.getItem('debug');
-const sbeekHave               = localStorage.getItem('sbeeker');
-const wrongHave               = localStorage.getItem('wrongpass');//
-const puresHave               = localStorage.getItem('pure');
-const fightClassicSFX         = localStorage.getItem('fcgSFX');
-
-///////Como o Usuario Deixou ao Iniciar///////// (Secret Status)
-const debugStatus            = localStorage.getItem('debugOn');
-const sbeekerStatus          = localStorage.getItem('sbeekerOn');
-const senhaStatus            = localStorage.getItem('senhaOn');
-const pureStatus             = localStorage.getItem('puresOn');
-const fightClassicSFXStatus  = localStorage.getItem('fcgSFXOn');
-
 ////////////////////////////////////////////////////////////////////////
 
 //  //////////Ativação na tela de password////////////  //
