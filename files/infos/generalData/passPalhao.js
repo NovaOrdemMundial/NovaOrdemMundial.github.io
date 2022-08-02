dc511c6789da3fdaf851bf2b0023c9e58f04ad33
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-08-02 05:29:03 -0300

    Update passPalhao.js

--- files/infos/generalData/passPalhao.js
+++ files/infos/generalData/passPalhao.js
@@ -1,3 +1,19 @@
+//////////////////SECRETS OF ABOUT US///////////// < -- Arquivo De Senhas
+const debugHave               = localStorage.getItem('debug');
+const sbeekHave               = localStorage.getItem('sbeeker');
+const wrongHave               = localStorage.getItem('wrongpass');//
+const puresHave               = localStorage.getItem('pure');
+const fightClassicSFX         = localStorage.getItem('fcgSFX');
+
+///////Como o Usuario Deixou ao Iniciar///////// (Secret Status)
+const debugStatus            = localStorage.getItem('debugOn');
+const sbeekerStatus          = localStorage.getItem('sbeekerOn');
+const senhaStatus            = localStorage.getItem('senhaOn');
+const pureStatus             = localStorage.getItem('puresOn');
+const fightClassicSFXStatus  = localStorage.getItem('fcgSFXOn');
+
+////////////////////////////////////////////////////////////////////////////
+
 const passwordCascad = [
   padrao = {
     possui:     'true',
