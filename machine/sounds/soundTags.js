ff313e29fc93af8a55e01af1eb31191a09c28aa5
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-08-03 05:10:52 -0300

    Update soundTags.js

--- machine/sounds/soundTags.js
+++ machine/sounds/soundTags.js
@@ -3,12 +3,12 @@ const maxVolume       = document.getElementById('VolumeMax');
 const audioVolume     = document.createElement('INPUT'); audioVolume.value = maxVolume.value;
 const audioLimiter    = ('000'+audioVolume.value);
 const volumeFilter    = '0.'+audioLimiter;
-const hoverSFX        = new Audio(sfx.button_hover);   hoverSFX.volume = 0.3;
-const clickSFX        = new Audio(sfx.button_click);   clickSFX.volume = 0.3;
-const backSFX         = new Audio(sfx.button_back);
-const closeSFX        = new Audio(sfx.button_close);
-const confirmSFX      = new Audio(sfx.button_confirm);
-const deniedSFX       = new Audio(sfx.button_deny);    deniedSFX.volume = 0.3;
+const hoverSFX        = new Audio(sfx.button_hover);    hoverSFX.volume = 0.3;
+const clickSFX        = new Audio(sfx.button_click);    clickSFX.volume = 0.3;
+const backSFX         = new Audio(sfx.button_back);     backSFX.volume = 0.3;
+const closeSFX        = new Audio(sfx.button_close);    closeSFX.volume = 0.3;
+const confirmSFX      = new Audio(sfx.button_confirm);  confirmSFX.volume = 0.3;
+const deniedSFX       = new Audio(sfx.button_deny);     deniedSFX.volume = 0.3;
 
 
 
