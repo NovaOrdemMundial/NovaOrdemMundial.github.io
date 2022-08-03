2ff6c83fdc96e98744e2cb706f7ab0798afae3a1
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-08-03 04:32:07 -0300

    Update soundmap.js

--- machine/sounds/soundmap.js
+++ machine/sounds/soundmap.js
@@ -6,7 +6,7 @@ const soudpack = [
     button_back:    '/audio/sfx/Silence.mp3',
     button_close:   '/audio/sfx/Silence.mp3',
     button_confirm: '/audio/sfx/Silence.mp3',
-	  button_deny:	  '/audio/sfx/Silence.mp3',
+    button_deny:    '/audio/sfx/Silence.mp3',
   },
   soundtracks = {
     sacrifice_button: 'https://novaordemmundial.github.io/audio/musics/Looped%20Instrumental%20Run%20Boy%20Run.mp3',
