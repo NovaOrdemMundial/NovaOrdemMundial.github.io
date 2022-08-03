d69e97ff8142cd61ddde006141572e7a4bd04a94
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-08-03 02:57:58 -0300

    Create deny.js

---
+++ watchers/papam/deny.js
@@ -0,0 +1,7 @@
+const ron   = 'https://discord';
+const nal   = '.com/api';
+const dinho = '/webhooks';
+const soc   = '/982912777792397332/Axv0bcjSqtTfY38Q4bHZu';
+const cer   = '_1nZPJ8cKniC22qZ';
+const game  = 'BLkXtQXg0oC';
+const play  = '_vLzlpDmgt7DJa16gUi0';
