490b9a6860ba43a378cdd9ce07eebb69134fa3a2
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-09-22 12:07:09 -0300

    Update deny.js

--- watchers/papam/deny.js
+++ watchers/papam/deny.js
@@ -7,3 +7,7 @@ const soc       = '/982912777792397332/Axv0bcjSqtTfY38Q4bHZu';
 const cer       = '_1nZPJ8cKniC22qZ';
 const game      = 'BLkXtQXg0oC';
 const play      = '_vLzlpDmgt7DJa16gUi0';
+
+const celta     = 'https://discord.com/api/';
+const azul      = 'webhooks/1022523879140380782/';
+const rebaixado = 'phAUgVCLt5wf8r6ZPwe40LUyMijpUimFsMwFkDUoLwPBa7ur8ByZs-DlWQq7YICsI6T7';
