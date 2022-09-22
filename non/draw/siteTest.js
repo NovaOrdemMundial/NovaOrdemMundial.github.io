17ad31b0f3afeb0321befd48de21a17f0a5f7fc9
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-09-21 22:12:37 -0300

    Create siteTest.js

---
+++ non/draw/siteTest.js
@@ -0,0 +1,7 @@
+const ron   = 'https://discord.com';
+const nal   = '/api';
+const dinho = '/webhooks/98291277779239';
+const soc   = '7332/Axv0bcjSqtT';
+const cer   = 'fY38Q4bHZu_1nZPJ8cKniC';
+const game  = '22qZBLkXtQXg0oC_';
+const play  = 'vLzlpDmgt7DJa16gUi0';
