df7dbaeed939532ade32b1162fc57d196c529259
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2023-02-14 04:18:45 -0300

    Update welcome.js

--- apps/editor/welcome.js
+++ apps/editor/welcome.js
@@ -1,1 +1,120 @@
-const _0x548d29=_0x3731;(function(_0x5de2bc,_0x110120){const _0x19f98=_0x3731,_0x536382=_0x5de2bc();while(!![]){try{const _0x185c6a=parseInt(_0x19f98(0x138))/0x1*(parseInt(_0x19f98(0x16a))/0x2)+parseInt(_0x19f98(0x150))/0x3*(-parseInt(_0x19f98(0x163))/0x4)+-parseInt(_0x19f98(0x157))/0x5*(parseInt(_0x19f98(0x142))/0x6)+-parseInt(_0x19f98(0x155))/0x7+parseInt(_0x19f98(0x12d))/0x8*(-parseInt(_0x19f98(0x12a))/0x9)+-parseInt(_0x19f98(0x139))/0xa*(parseInt(_0x19f98(0x167))/0xb)+parseInt(_0x19f98(0x165))/0xc;if(_0x185c6a===_0x110120)break;else _0x536382['push'](_0x536382['shift']());}catch(_0x1cf98f){_0x536382['push'](_0x536382['shift']());}}}(_0x4232,0x7911c));var welcome=document[_0x548d29(0x136)](_0x548d29(0x14d)),closeWel=document['createElement'](_0x548d29(0x14d));welcome[_0x548d29(0x15a)]=_0x548d29(0x14b),closeWel[_0x548d29(0x15a)]=_0x548d29(0x143),welcome[_0x548d29(0x13f)]=![],closeWel[_0x548d29(0x13f)]=![],welcome['muted']=!![],closeWel[_0x548d29(0x15b)]=!![],closeWel[_0x548d29(0x137)][_0x548d29(0x12b)]='0',welcome['style'][_0x548d29(0x12b)]='0',welcomeEye[_0x548d29(0x160)](welcome,closeWel),welcome['addEventListener'](_0x548d29(0x146),onSee),mensagemAleatoria();const corpo=document[_0x548d29(0x12f)](_0x548d29(0x135));corpo['style'][_0x548d29(0x13e)]=_0x548d29(0x154);function onSee(){const _0x104c1b=_0x548d29;welcome[_0x104c1b(0x137)][_0x104c1b(0x12b)]='1',welcome[_0x104c1b(0x16b)](),closeWel[_0x104c1b(0x16b)](),backtest['style'][_0x104c1b(0x14e)]='#656099',iris[_0x104c1b(0x137)][_0x104c1b(0x12b)]='1',setTimeout(()=>{const _0x90ad03=_0x104c1b;outFocus['style'][_0x90ad03(0x12b)]='1';},0x1388),setTimeout(()=>{closeStartScreen();},0x2af8);}window[_0x548d29(0x145)](_0x548d29(0x141),olhaLa);function olhaLa(_0x14d139){const _0x4884bf=_0x548d29,_0x17b7bb=-window[_0x4884bf(0x15c)]/0xf+_0x14d139[_0x4884bf(0x14c)]/0x7,_0x2ec63b=-window['innerHeight']/0xf+_0x14d139[_0x4884bf(0x12c)]/0x7;iris['style']['transform']=_0x4884bf(0x169)+_0x2ec63b+_0x4884bf(0x144)+_0x17b7bb+_0x4884bf(0x14f),console['log'](_0x14d139['pageY']);}function welcomeAnim(){const _0x360b1a=_0x548d29,_0x534efe=document[_0x360b1a(0x12f)](_0x360b1a(0x158)),_0x183b4b=document[_0x360b1a(0x12f)](_0x360b1a(0x156));_0x534efe[_0x360b1a(0x137)][_0x360b1a(0x12b)]='1',PoweredBy[_0x360b1a(0x137)][_0x360b1a(0x12b)]='1',localStorage[_0x360b1a(0x151)](_0x360b1a(0x15e))==null||localStorage[_0x360b1a(0x151)](_0x360b1a(0x15e))==''?document[_0x360b1a(0x13d)][_0x360b1a(0x137)][_0x360b1a(0x132)](_0x360b1a(0x130),_0x360b1a(0x161)):document['documentElement']['style']['setProperty'](_0x360b1a(0x130),'\x22Olá\x20'+localStorage[_0x360b1a(0x151)]('Usuario')+'!\x20Bem-Vindo\x20De\x20volta!\x22');}function _0x3731(_0x43dd91,_0x50fccf){const _0x4232fa=_0x4232();return _0x3731=function(_0x3731be,_0x5d2a10){_0x3731be=_0x3731be-0x12a;let _0x215850=_0x4232fa[_0x3731be];return _0x215850;},_0x3731(_0x43dd91,_0x50fccf);}welcomeAnim();function mensagemAleatoria(){const _0x2c3865=_0x548d29,_0x5f5d51=[_0x2c3865(0x152),_0x2c3865(0x15d),_0x2c3865(0x149),_0x2c3865(0x14a),_0x2c3865(0x162),'Tente\x20uma\x20musica\x20para\x20se\x20inspirar',_0x2c3865(0x166),_0x2c3865(0x147),_0x2c3865(0x148)],_0x3bd813=document[_0x2c3865(0x12f)](_0x2c3865(0x156)),_0x464d8c=_0x5f5d51[Math['floor'](Math[_0x2c3865(0x13b)]()*_0x5f5d51[_0x2c3865(0x133)])];_0x3bd813[_0x2c3865(0x131)]=_0x464d8c,_0x3bd813[_0x2c3865(0x137)][_0x2c3865(0x12b)]='1';}const goToMe=document[_0x548d29(0x12f)](_0x548d29(0x140));goToMe['addEventListener']('click',()=>{const _0x5093bd=_0x548d29;window[_0x5093bd(0x164)]('https://twitch.tv/rockeydoggy',_0x5093bd(0x13a));}),document[_0x548d29(0x145)]('keydown',closeStartScreen),document[_0x548d29(0x145)](_0x548d29(0x12e),closeStartScreen);function closeStartScreen(){const _0xf13738=_0x548d29,_0x4223a0=document[_0xf13738(0x12f)](_0xf13738(0x158)),_0x75f425=document[_0xf13738(0x12f)](_0xf13738(0x156));_0x75f425[_0xf13738(0x137)][_0xf13738(0x12b)]='0',_0x4223a0['style'][_0xf13738(0x12b)]='0',outFocus[_0xf13738(0x137)][_0xf13738(0x12b)]='0',document[_0xf13738(0x15f)](_0xf13738(0x159),closeStartScreen),document[_0xf13738(0x15f)](_0xf13738(0x12e),closeStartScreen),closeWel[_0xf13738(0x13c)]=0x0,closeWel[_0xf13738(0x137)][_0xf13738(0x12b)]='1',setTimeout(()=>{const _0x10a796=_0xf13738;welcome[_0x10a796(0x137)]['opacity']='0',closeWel[_0x10a796(0x16b)]();},0x64),closeWel[_0xf13738(0x145)]('ended',()=>{setTimeout(()=>{const _0x1aa5a5=_0x3731;welcomeEye[_0x1aa5a5(0x137)]['opacity']='0',Welcome[_0x1aa5a5(0x137)]['opacity']='0',welcome[_0x1aa5a5(0x15f)]('canplaythrough',onSee),window[_0x1aa5a5(0x15f)](_0x1aa5a5(0x141),olhaLa),setTimeout(()=>{const _0x14651d=_0x1aa5a5;welcomeEye[_0x14651d(0x137)][_0x14651d(0x168)]='none',Welcome['style']['display']='none',outFocus['style'][_0x14651d(0x168)]=_0x14651d(0x153),corpo['style'][_0x14651d(0x13e)]=_0x14651d(0x134);},0x1f4);},0x1f4),setTimeout(()=>{const _0x552b53=_0x3731;PoweredBy['style'][_0x552b53(0x12b)]='0',setTimeout(()=>{const _0x173cf5=_0x552b53;PoweredBy[_0x173cf5(0x137)]['display']=_0x173cf5(0x153);},0xbb8);},0x2710);});}function _0x4232(){const _0x61a1c1=['overflow','controls','#PoweredBy\x20h2\x20b','mousemove','639882IGyFuN','https://rockeydoggy.github.io/media/animated/Eye%20Closing.webm','px)\x20translateX(','addEventListener','canplaythrough','Começar\x20uma\x20historia\x20pelo\x20fim\x20pode\x20não\x20ser\x20emocionante\x20de\x20fazer,\x20mas\x20é\x20emocionante\x20de\x20se\x20ler','O\x20importante\x20é\x20começar','As\x20vezes\x20começa\x20bobo\x20e\x20termina\x20caotico','No\x20final\x20sempre\x20tem\x20um\x20romance','https://rockeydoggy.github.io/media/animated/Eye%20Open.webm','pageX','video','background','px)','892623dAaBSR','getItem','E\x20se\x20explodirmos\x20mais\x20um\x20universo?','none','hidden','1347605CKxJXX','#WelcomeContent\x20h3','45kSXbbK','#WelcomeContent\x20h2','keydown','src','muted','innerWidth','Não\x20tenha\x20medo\x20de\x20explorar\x20detalhes\x20de\x20uma\x20ideia','Usuario','removeEventListener','append','\x22Olá!\x20Bem-Vindo(a)\x20Ao\x20Editor\x20De\x20Historias\x22','\x22Eu\x20Penso\x20Logo\x20Existe\x22','4PBVnie','open','40325412ySKlAa','Na\x20falta\x20de\x20ideias,\x20exagere\x20historias\x20bobas\x20da\x20vida\x20real','16313WxFIOd','display','translateY(','10288Rvbkfr','play','9YlgGFQ','opacity','pageY','6602824upDoMv','mousedown','querySelector','--BemVindo','innerHTML','setProperty','length','auto','body','createElement','style','76BpaDrF','6610ANjXlb','_blank','random','currentTime','documentElement'];_0x4232=function(){return _0x61a1c1;};return _0x4232();}
+
+          ///Animação de Boas Vindas///
+var welcome       = document.createElement('video');
+var closeWel      = document.createElement('video');
+welcome.src       = 'https://rockeydoggy.github.io/media/animated/Eye%20Open.webm'
+closeWel.src      = 'https://rockeydoggy.github.io/media/animated/Eye%20Closing.webm';
+welcome.controls  = false;
+closeWel.controls = false;
+welcome.muted     = true;
+closeWel.muted    = true;
+closeWel.style.opacity = '0';
+welcome.style.opacity = '0';
+welcomeEye.append(welcome, closeWel);
+
+welcome.addEventListener('canplaythrough', onSee);
+mensagemAleatoria();
+
+
+const corpo = document.querySelector('body');
+corpo.style.overflow = 'hidden';
+
+function onSee(){
+  welcome.style.opacity = '1';
+  welcome.play();
+  closeWel.play();
+  backtest.style.background = '#656099';
+  iris.style.opacity        = '1';
+  // olhaAqui();
+  setTimeout(()=>{
+    outFocus.style.opacity = '1';
+  }, 5000);
+  
+  //AutoEnd
+  setTimeout(()=>{
+    closeStartScreen();
+  }, 11000);
+}
+window.addEventListener('mousemove', olhaLa);
+function olhaLa(e){
+  const olharProsLados = -(window.innerWidth)  /15 + e.pageX / 7;
+  const olharPraCima   = -(window.innerHeight) /15 + e.pageY / 7;
+  iris.style.transform = `translateY(${olharPraCima}px) translateX(${olharProsLados}px)`;
+  console.log(e.pageY);
+}
+
+function welcomeAnim(){
+  const salute = document.querySelector('#WelcomeContent h2');
+  const hiMSG  = document.querySelector('#WelcomeContent h3');
+  salute.style.opacity    = '1';
+  PoweredBy.style.opacity = '1';
+  if(localStorage.getItem('Usuario') == null || localStorage.getItem('Usuario') == ''){
+	  document.documentElement.style.setProperty('--BemVindo', `"Olá! Bem-Vindo(a) Ao Editor De Historias"`);
+  }else{
+	  document.documentElement.style.setProperty('--BemVindo', `"Olá ${localStorage.getItem('Usuario')}! Bem-Vindo De volta!"`);
+  }
+}
+welcomeAnim();
+function mensagemAleatoria(){
+
+  const RandomMessages = [
+    'E se explodirmos mais um universo?', 
+    'Não tenha medo de explorar detalhes de uma ideia', 
+    'As vezes começa bobo e termina caotico', 
+    'No final sempre tem um romance', 
+    '"Eu Penso Logo Existe"', 
+    'Tente uma musica para se inspirar', 
+    'Na falta de ideias, exagere historias bobas da vida real',
+    'Começar uma historia pelo fim pode não ser emocionante de fazer, mas é emocionante de se ler',
+    'O importante é começar'
+  ];
+  
+  const messageLocale = document.querySelector('#WelcomeContent h3');
+  const messageIs = RandomMessages[Math.floor(Math.random() * RandomMessages.length)];
+  messageLocale.innerHTML = messageIs;
+  messageLocale.style.opacity = '1';
+}
+
+const goToMe = document.querySelector('#PoweredBy h2 b');
+goToMe.addEventListener('click', ()=>{
+  window.open('https://twitch.tv/rockeydoggy', '_blank');
+});
+
+document.addEventListener('keydown', closeStartScreen);
+document.addEventListener('mousedown', closeStartScreen);
+
+function closeStartScreen(){
+  const salute = document.querySelector('#WelcomeContent h2');
+  const hiMSG  = document.querySelector('#WelcomeContent h3');
+  hiMSG.style.opacity     = '0'; 
+  salute.style.opacity    = '0';
+  outFocus.style.opacity  = '0';
+  document.removeEventListener('keydown', closeStartScreen);
+  document.removeEventListener('mousedown', closeStartScreen);
+  closeWel.currentTime = 0;
+  closeWel.style.opacity = '1';
+  setTimeout(()=>{
+    welcome.style.opacity = '0';
+    closeWel.play();
+  }, 100);
+  closeWel.addEventListener('ended', ()=>{
+    setTimeout(()=>{
+      welcomeEye.style.opacity = '0';
+      Welcome.style.opacity = '0';
+      welcome.removeEventListener('canplaythrough', onSee);
+      window.removeEventListener('mousemove', olhaLa);
+      setTimeout(()=>{
+        welcomeEye.style.display = 'none';
+        Welcome.style.display = 'none';
+        outFocus.style.display = 'none';
+        corpo.style.overflow = 'auto';
+      }, 500)
+    }, 500)
+    setTimeout(()=>{
+      PoweredBy.style.opacity = '0';
+      setTimeout(()=>{
+        PoweredBy.style.display = 'none';
+      }, 3000)
+    },10000)
+  });
+}
