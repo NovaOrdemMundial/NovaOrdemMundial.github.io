51135c2afa2a270190aaf7a027bf92d73655395a
NovaOrdemMundial <91165350+NovaOrdemMundial@users.noreply.github.com>
2023-05-24 01:47:54 -0300

    Add files via upload

---
+++ apps/mods/css-js/tramission.js
@@ -0,0 +1,11 @@
+﻿const firebaseConfig = {
+  apiKey: "AIzaSyDdOKtJq-s7ZeAFL3hw1XOU6wUmdlZpE9c",
+  authDomain: "modpacklist.firebaseapp.com",
+  projectId: "modpacklist",
+  storageBucket: "modpacklist.appspot.com",
+  messagingSenderId: "367199827849",
+  appId: "1:367199827849:web:0037d2acd710805c1d1358",
+  measurementId: "G-THWMHCT57E"
+};
+firebase.initializeApp(firebaseConfig);
+const bankOf = firebase.firestore();
